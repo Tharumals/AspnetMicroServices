4163437f2f0257cea3faec6efa7cd8967d24291b
Nadeeka2 <nadeeka@dspcontrols.lk>
2021-11-28 19:37:20 +0530

    Catalog.API initial

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepositiry _repositiry;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepositiry repositiry, ILogger<CatalogController> logger)
+        {
+            _repositiry = repositiry;
+            _logger = logger;
+        }
+    }
+}
